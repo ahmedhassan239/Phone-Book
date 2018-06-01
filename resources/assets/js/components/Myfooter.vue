de1c6d0a198072c5e8dc07a6fb35d355bb1102fa
ahmed <ahmed25.ah@gmail.com>
2018-06-01 07:42:29 +0200

    install font awesome and make home and about template

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+	<footer class="footer">
+	  <div class="container">
+	    <div class="content has-text-centered">
+	      <p>
+	        <strong>Vuejs Phonebook App</strong> by <a href="https://github.com/ahmedhassan239/Phone-Book">Ahmed Hassan 2018</a>
+	      </p>
+	    </div>
+	  </div>
+	</footer>
+</template>
